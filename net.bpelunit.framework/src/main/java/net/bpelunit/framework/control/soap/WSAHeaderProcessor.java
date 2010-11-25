fee9b396686f88f2d736c555177698af252682a4
unknown <dluebke@.(none)>
2010-11-25 11:18:28 +0100

    First WSA Header Processor Updates

--- net.bpelunit.framework/src/main/java/net/bpelunit/framework/control/soap/WSAHeaderProcessor.java
+++ net.bpelunit.framework/src/main/java/net/bpelunit/framework/control/soap/WSAHeaderProcessor.java
@@ -5,9 +5,7 @@
  */
 package net.bpelunit.framework.control.soap;
 
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
 
 import javax.xml.namespace.QName;
 import javax.xml.soap.SOAPElement;
@@ -21,34 +19,46 @@ import net.bpelunit.framework.exception.HeaderProcessingException;
 import net.bpelunit.framework.model.test.activity.ActivityContext;
 
 /**
- * This class implements the WS-Adressing asynchronous call handling, as specified in the WS-A
- * 2003/03 standard.
+ * This class implements the WS-Adressing asynchronous call handling, as
+ * specified in the WS-A 2003/03 standard.
+ * 
+ * Added 2004 version and many more options (dluebke)
  * 
  * @version $Id$
- * @author Philip Mayer
+ * @author Philip Mayer, Daniel Luebke
  * 
  */
 public class WSAHeaderProcessor implements IHeaderProcessor {
 
-	private static final String WSA_TAG_RELATES_TO= "RelatesTo";
-	private static final String WSA_TAG_ADDRESS= "Address";
-	private static final String WSA_TAG_REPLY_TO= "ReplyTo";
-	private static final String WSA_TAG_MESSAGE_ID= "MessageID";
-	private static final String WSA_SENT_DEFAULT_ID= "WSA-838848474774-3883873747";
-	private static final String WSA_NAMESPACE= "http://schemas.xmlsoap.org/ws/2003/03/addressing";
-	private static final String WSA_RECEIVED= "WSA-Received";
-	private static final String WSA_RECEIVED_ADDRESS= "WSA-Received-Address";
-	private static final String WSA_RECEIVED_ID= "WSA-Received-ID";
-	private static final String WSA_SENT= "WSA-Sent";
-	private static final String WSA_SENT_ID= "WSA-Sent-ID";
+	private static final String WSA_TAG_RELATES_TO = "RelatesTo";
+	private static final String WSA_TAG_ADDRESS = "Address";
+	private static final String WSA_TAG_REPLY_TO = "ReplyTo";
+	private static final String WSA_TAG_FAULT_TO = "FaultTo";
+	private static final String WSA_TAG_MESSAGE_ID = "MessageID";
+	private static final String WSA2003_NAMESPACE = "http://schemas.xmlsoap.org/ws/2003/03/addressing";
+	private static final String WSA2004_NAMESPACE = "http://schemas.xmlsoap.org/ws/2004/08/addressing";
+	private static final String WSA_RECEIVED = "WSA-Received";
+	private static final String WSA_RECEIVED_ADDRESS = "WSA-Received-Address";
+	private static final String WSA_RECEIVED_ID = "WSA-Received-ID";
+	private static final String WSA_SENT = "WSA-Sent";
+	private static final String WSA_SENT_ID = "WSA-Sent-ID";
 
-	private Map<String, String> fProperties;
+	private String messageId = "BPELUnit-"
+			+ Math.abs((long)Math.random() * 100000000);
+	private String expectedMessageId = null;
+	private String expectedRelatesTo = null; // filled in when message is sent
+	private boolean validateRelatesTo = false;
+	private String wsaNamespace = WSA2003_NAMESPACE;
+	private String replyToPartnerTrack = null;
+	private String replyToURI = null;
+	private String faultToPartnerTrack = null;
+	private String faultToURI = null;
 
 	public WSAHeaderProcessor() {
-		fProperties= new HashMap<String, String>();
 	}
 
-	public void processReceive(ActivityContext context, SOAPMessage message) throws HeaderProcessingException {
+	public void processReceive(ActivityContext context, SOAPMessage message)
+			throws HeaderProcessingException {
 
 		// Two options: Either this is part one in a receive-send, or part two
 		// in a send/receive
@@ -61,34 +71,40 @@ public class WSAHeaderProcessor implements IHeaderProcessor {
 			// get message ID and reply-To-Adress
 			SOAPHeader header;
 			try {
-				header= message.getSOAPHeader();
+				header = message.getSOAPHeader();
 			} catch (SOAPException e) {
-				throw new HeaderProcessingException("Incoming SOAP message did not have a SOAP Header.", e);
+				throw new HeaderProcessingException(
+						"Incoming SOAP message did not have a SOAP Header.", e);
 			}
 
 			// Retrieve message ID
-			String messageID= null;
-			for (Iterator<?> i= header.getChildElements(new QName(WSA_NAMESPACE, WSA_TAG_MESSAGE_ID)); i.hasNext();) {
-				SOAPElement soapElement= (SOAPElement) i.next();
-				messageID= soapElement.getTextContent();
+			String messageID = null;
+			for (Iterator<?> i = header.getChildElements(new QName(
+					wsaNamespace, WSA_TAG_MESSAGE_ID)); i.hasNext();) {
+				SOAPElement soapElement = (SOAPElement) i.next();
+				messageID = soapElement.getTextContent();
 			}
 
 			// Retrieve reply to
-			String replyTo= null;
-			for (Iterator<?> i= header.getChildElements(new QName(WSA_NAMESPACE, WSA_TAG_REPLY_TO)); i.hasNext();) {
-				SOAPElement soapElement= (SOAPElement) i.next();
-				for (Iterator<?> j= soapElement.getChildElements(new QName(WSA_NAMESPACE, WSA_TAG_ADDRESS)); j.hasNext();) {
-					SOAPElement soapElement2= (SOAPElement) j.next();
-					replyTo= soapElement2.getTextContent();
+			String replyTo = null;
+			for (Iterator<?> i = header.getChildElements(new QName(
+					wsaNamespace, WSA_TAG_REPLY_TO)); i.hasNext();) {
+				SOAPElement soapElement = (SOAPElement) i.next();
+				for (Iterator<?> j = soapElement.getChildElements(new QName(
+						wsaNamespace, WSA_TAG_ADDRESS)); j.hasNext();) {
+					SOAPElement soapElement2 = (SOAPElement) j.next();
+					replyTo = soapElement2.getTextContent();
 				}
 			}
 
 			if (messageID == null)
-				messageID= "";
-			// throw new HeaderProcessingException("Message ID not found in incoming message.");
+				messageID = "";
+			// throw new
+			// HeaderProcessingException("Message ID not found in incoming message.");
 
 			if (replyTo == null)
-				throw new HeaderProcessingException("Reply-To address not found in incoming message.");
+				throw new HeaderProcessingException(
+						"Reply-To address not found in incoming message.");
 
 			context.setUserData(WSA_RECEIVED, "true");
 			context.setUserData(WSA_RECEIVED_ID, messageID);
@@ -96,35 +112,42 @@ public class WSAHeaderProcessor implements IHeaderProcessor {
 		}
 	}
 
-	public void processSend(ActivityContext context, SendPackage sendSpec) throws HeaderProcessingException {
+	public void processSend(ActivityContext context, SendPackage sendSpec)
+			throws HeaderProcessingException {
 
 		// Two options: Either this is part one in a send/receive, or part two
 		// in a receive/send.
 		if (context.getUserData(WSA_RECEIVED).equals("true")) {
 			// we are sending back
-			String targetURL= context.getUserData(WSA_RECEIVED_ADDRESS);
-			String messageID= context.getUserData(WSA_RECEIVED_ID);
+			String targetURL = context.getUserData(WSA_RECEIVED_ADDRESS);
+			String messageID = context.getUserData(WSA_RECEIVED_ID);
 
 			if (targetURL == null)
-				throw new HeaderProcessingException("Target URL from presumed receive was empty.");
+				throw new HeaderProcessingException(
+						"Target URL from presumed receive was empty.");
 
 			if (messageID == null)
-				throw new HeaderProcessingException("Message ID from presumed receive was empty.");
+				throw new HeaderProcessingException(
+						"Message ID from presumed receive was empty.");
 
-			SOAPMessage msg= sendSpec.getSoapMessage();
+			SOAPMessage msg = sendSpec.getSoapMessage();
 			SOAPHeader header;
 			try {
-				header= msg.getSOAPHeader();
+				header = msg.getSOAPHeader();
 			} catch (SOAPException e) {
-				throw new HeaderProcessingException("No SOAP header in outgoing SOAP message.", e);
+				throw new HeaderProcessingException(
+						"No SOAP header in outgoing SOAP message.", e);
 			}
 
 			if (!messageID.equals("")) {
 				SOAPElement msgId;
 				try {
-					msgId= header.addChildElement(new QName(WSA_NAMESPACE, WSA_TAG_RELATES_TO));
+					msgId = header.addChildElement(new QName(wsaNamespace,
+							WSA_TAG_RELATES_TO));
 				} catch (SOAPException e) {
-					throw new HeaderProcessingException("Could not add relatesTo child element to outgoing SOAP message.", e);
+					throw new HeaderProcessingException(
+							"Could not add relatesTo child element to outgoing SOAP message.",
+							e);
 				}
 				msgId.setTextContent(messageID);
 			}
@@ -134,37 +157,114 @@ public class WSAHeaderProcessor implements IHeaderProcessor {
 		} else {
 			// we are first, so this is a send-receive activity
 			// add reply-to and message ID to the SOAP message
-			SOAPMessage msg= sendSpec.getSoapMessage();
+			SOAPMessage msg = sendSpec.getSoapMessage();
 			SOAPHeader header;
 			try {
-				header= msg.getSOAPHeader();
+				header = msg.getSOAPHeader();
 			} catch (SOAPException e) {
-				throw new HeaderProcessingException("No SOAP header in outgoing SOAP message.", e);
+				throw new HeaderProcessingException(
+						"No SOAP header in outgoing SOAP message.", e);
 			}
 
 			try {
 				// Message ID
-				String id= WSA_SENT_DEFAULT_ID;
-				SOAPElement msgId= header.addChildElement(new QName(WSA_NAMESPACE, WSA_TAG_MESSAGE_ID));
-				msgId.setTextContent(id);
+				SOAPElement msgId = header.addChildElement(new QName(
+						wsaNamespace, WSA_TAG_MESSAGE_ID));
+				msgId.setTextContent(messageId);
 
 				// Reply To
-				SOAPElement replyTo= header.addChildElement(new QName(WSA_NAMESPACE, WSA_TAG_REPLY_TO));
-				SOAPElement address= replyTo.addChildElement(new QName(WSA_NAMESPACE, WSA_TAG_ADDRESS));
+				SOAPElement replyTo = header.addChildElement(new QName(
+						wsaNamespace, WSA_TAG_REPLY_TO));
+				SOAPElement address = replyTo.addChildElement(new QName(
+						wsaNamespace, WSA_TAG_ADDRESS));
 
 				address.setTextContent(context.getPartnerURL());
 
 				// store this
 				context.setUserData(WSA_SENT, "true");
-				context.setUserData(WSA_SENT_ID, id);
+				context.setUserData(WSA_SENT_ID, messageId);
 			} catch (SOAPException e) {
-				throw new HeaderProcessingException("Could not add MessageID, ReplyTo or Address child element to outgoing SOAP message.", e);
+				throw new HeaderProcessingException(
+						"Could not add MessageID, ReplyTo or Address child element to outgoing SOAP message.",
+						e);
 			}
 		}
 	}
 
-	public void setProperty(String name, String stringValue) {
-		fProperties.put(name, stringValue);
+	public void setMessageId(String messageId) {
+		this.messageId = messageId;
 	}
 
+	public void setWSAVersion(String version) {
+		if ("2003".equals(version) || "".equals(version)) {
+			wsaNamespace = WSA2003_NAMESPACE;
+		} else if ("2004".equals(version)) {
+			wsaNamespace = WSA2004_NAMESPACE;
+		} else {
+			throw new IllegalArgumentException(
+					"WS-Addressing Version may only be 2003 or 2004");
+		}
+	}
+
+	public void setProperty(String name, String value) {
+		if ("MessageId".equals(name)) {
+			setMessageId(value);
+		}
+
+		if ("WSAVersion".equals(name)) {
+			setWSAVersion(value);
+		}
+
+		if ("ExpectedMessageId".equals(name)) {
+			setExpectedMessageId(value);
+		}
+
+		if ("ExpectedRelatesTo".equals(name)) {
+			setExpectedRelatesTo(value);
+		}
+	}
+
+	public void setExpectedMessageId(String expectedMessageId) {
+		this.expectedMessageId = expectedMessageId;
+	}
+
+	public void setExpectedRelatesTo(String expectedRelateToId) {
+		this.expectedRelatesTo = expectedRelateToId;
+	}
+
+	public void setReplyToPartnerTrack(String replyToPartnerTrack) {
+		this.replyToPartnerTrack = replyToPartnerTrack;
+
+		if (replyToURI != null) {
+			throw new IllegalArgumentException(
+					"Only one of ReplyToPartnerTrack and ReplyToURL is allowed.");
+		}
+	}
+
+	public void setReplyToURI(String replyToURL) {
+		this.replyToURI = replyToURL;
+
+		if (replyToPartnerTrack != null) {
+			throw new IllegalArgumentException(
+					"Only one of ReplyToPartnerTrack and ReplyToURL is allowed.");
+		}
+	}
+
+	public void setFaultToPartnerTrack(String faultToPartnerTrack) {
+		this.faultToPartnerTrack = faultToPartnerTrack;
+
+		if (faultToURI != null) {
+			throw new IllegalArgumentException(
+					"Only one of FaultToPartnerTrack and FaultToURL is allowed.");
+		}
+	}
+
+	public void setFaultToURI(String faultToURL) {
+		this.faultToURI = faultToURL;
+
+		if (faultToPartnerTrack != null) {
+			throw new IllegalArgumentException(
+					"Only one of FaultToPartnerTrack and FaultToURL is allowed.");
+		}
+	}
 }
--- net.bpelunit.framework/src/test/java/net/bpelunit/test/unit/TestWSAHeaderProcessor.java
+++ net.bpelunit.framework/src/test/java/net/bpelunit/test/unit/TestWSAHeaderProcessor.java
@@ -5,17 +5,26 @@
  */
 package net.bpelunit.test.unit;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
 
 import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPMessage;
+import javax.xml.xpath.XPathExpressionException;
 
 import net.bpelunit.framework.control.ext.IHeaderProcessor;
 import net.bpelunit.framework.control.ext.SendPackage;
 import net.bpelunit.framework.control.soap.NamespaceContextImpl;
 import net.bpelunit.framework.control.soap.WSAHeaderProcessor;
+import net.bpelunit.framework.exception.HeaderProcessingException;
 import net.bpelunit.framework.model.test.activity.ActivityContext;
 import net.bpelunit.test.util.TestUtil;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -24,77 +33,146 @@ import org.w3c.dom.Node;
  * 
  * Tests the WSA Header Processor
  * 
- * @version $Id: TestWSAHeaderProcessor.java,v 1.5 2006/07/11 14:27:43 phil Exp $
+ * @version $Id: TestWSAHeaderProcessor.java,v 1.5 2006/07/11 14:27:43 phil Exp
+ *          $
  * @author Philip Mayer
  * 
  */
 public class TestWSAHeaderProcessor extends SimpleTest {
 
-	private static final String PATH_TO_FILES= "/wsahp/";
+	private static final String NAMESPACE_WSA_2003 = "http://schemas.xmlsoap.org/ws/2003/03/addressing";
 
+	private static final String NAMESPACE_WSA_2004 = "http://schemas.xmlsoap.org/ws/2004/08/addressing";
+
+	private static final String PATH_TO_FILES = "/wsahp/";
+
+	ActivityContext activityContext;
+
+	private MessageFactory factory;
+
+	private WSAHeaderProcessor wsaProcessor;
+
+	@Before
+	public void before() throws SOAPException {
+		this.activityContext = new ActivityContext("http://simulated.url");
+		this.factory = MessageFactory.newInstance();
+		this.wsaProcessor = new WSAHeaderProcessor();
+	}
+	
 	@Test
-	public void testWSAHeadersReceiveSend() throws Exception {
-
-		ActivityContext dummyContext= new ActivityContext("http://simulated.url");
-
-		MessageFactory factory= MessageFactory.newInstance();
-		SOAPMessage rcvMessage= factory.createMessage(null, this.getClass().getResourceAsStream(PATH_TO_FILES + "incomingSOAP.xml"));
-
-		IHeaderProcessor proc= new WSAHeaderProcessor();
-
-		proc.processReceive(dummyContext, rcvMessage);
-
-		assertEquals("true", dummyContext.getUserData("WSA-Received"));
-		assertEquals("kjasdhfjsdbksjdgdsrhfdgjkdfjk", dummyContext.getUserData("WSA-Received-ID"));
-		assertEquals("http://return.to.me/", dummyContext.getUserData("WSA-Received-Address"));
-
-		SOAPMessage sendMessage= factory.createMessage(null, this.getClass().getResourceAsStream(PATH_TO_FILES + "outgoingSOAP.xml"));
-
-		SendPackage p= new SendPackage("http://target.url", sendMessage);
-		proc.processSend(dummyContext, p);
-
-		assertEquals("http://return.to.me/", p.getTargetURL());
-		Element header= p.getSoapMessage().getSOAPHeader();
-
-		NamespaceContextImpl ns= new NamespaceContextImpl();
-		ns.setNamespace("wsa", "http://schemas.xmlsoap.org/ws/2003/03/addressing");
-
-		Node msgId= TestUtil.getNode(header, ns, "wsa:RelatesTo");
-		assertEquals("kjasdhfjsdbksjdgdsrhfdgjkdfjk", msgId.getTextContent());
+	public void testWSA2003HeadersReceiveSend() throws Exception {
+		testWSAHeadersReceiveSend(NAMESPACE_WSA_2003);
 	}
 
 	@Test
-	public void testWSAHeadersSendReceive() throws Exception {
+	public void testWSA2004HeadersReceiveSend() throws Exception {
+		wsaProcessor.setWSAVersion("2004");
+		testWSAHeadersReceiveSend(NAMESPACE_WSA_2004);
+	}
 
-		ActivityContext dummyContext= new ActivityContext("http://simulated.url");
-		IHeaderProcessor proc= new WSAHeaderProcessor();
+	private void testWSAHeadersReceiveSend(String namespaceURI)
+			throws IOException, SOAPException, HeaderProcessingException,
+			XPathExpressionException {
+		
+		String messageId = "kjasdhfjsdbksjdgdsrhfdgjkdfjk";
+		
+		String fileName = "incomingSOAP.xml";
+		if(namespaceURI.equals(NAMESPACE_WSA_2004)) {
+			fileName = "incomingSOAP_WSA2004.xml";
+		}
+		
+		SOAPMessage rcvMessage = factory.createMessage(null, this.getClass()
+				.getResourceAsStream(PATH_TO_FILES + fileName));
 
-		MessageFactory factory= MessageFactory.newInstance();
-		SOAPMessage sendMessage= factory.createMessage(null, this.getClass().getResourceAsStream(PATH_TO_FILES + "outgoingSOAP.xml"));
+		wsaProcessor.processReceive(activityContext, rcvMessage);
 
-		SendPackage p= new SendPackage("http://target.url", sendMessage);
-		proc.processSend(dummyContext, p);
+		assertEquals("true", activityContext.getUserData("WSA-Received"));
+		assertEquals(messageId,
+				activityContext.getUserData("WSA-Received-ID"));
+		assertEquals("http://return.to.me/",
+				activityContext.getUserData("WSA-Received-Address"));
+
+		SOAPMessage sendMessage = factory.createMessage(null, this.getClass()
+				.getResourceAsStream(PATH_TO_FILES + "outgoingSOAP.xml"));
+
+		SendPackage p = new SendPackage("http://target.url", sendMessage);
+		wsaProcessor.processSend(activityContext, p);
+
+		assertEquals("http://return.to.me/", p.getTargetURL());
+		Element header = p.getSoapMessage().getSOAPHeader();
+
+		NamespaceContextImpl ns = new NamespaceContextImpl();
+		ns.setNamespace("wsa",
+				namespaceURI);
+
+		Node msgId = TestUtil.getNode(header, ns, "wsa:RelatesTo");
+		assertEquals(messageId, msgId.getTextContent());
+	}
+
+	@Test
+	public void testWSA2003HeadersSendReceive() throws Exception {
+		testWSAHeadersSendReceive(NAMESPACE_WSA_2003);
+	}
+
+	@Test
+	public void testWSA2004HeadersSendReceive() throws Exception {
+		wsaProcessor.setWSAVersion("2004");
+		
+		testWSAHeadersSendReceive(NAMESPACE_WSA_2004);
+	}
+
+	private void testWSAHeadersSendReceive(String namespaceURI) throws IOException, SOAPException,
+			HeaderProcessingException, XPathExpressionException {
+		SOAPMessage sendMessage = factory.createMessage(null, this.getClass()
+				.getResourceAsStream(PATH_TO_FILES + "outgoingSOAP.xml"));
+
+		SendPackage p = new SendPackage("http://target.url", sendMessage);
+		wsaProcessor.processSend(activityContext, p);
 
 		/*
-		 * Processor needs to insert MessageID and Reply-To-Adress into the SOAP heaper.
+		 * Processor needs to insert MessageID and Reply-To-Address into the SOAP
+		 * header.
 		 */
 
-		Element header= p.getSoapMessage().getSOAPHeader();
-		NamespaceContextImpl ns= new NamespaceContextImpl();
-		ns.setNamespace("wsa", "http://schemas.xmlsoap.org/ws/2003/03/addressing");
-		Node msgId= TestUtil.getNode(header, ns, "wsa:MessageID");
-		assertEquals("WSA-838848474774-3883873747", msgId.getTextContent());
-		Node replyTo= TestUtil.getNode(header, ns, "wsa:ReplyTo/wsa:Address");
+		Element header = p.getSoapMessage().getSOAPHeader();
+		NamespaceContextImpl ns = new NamespaceContextImpl();
+		ns.setNamespace("wsa",
+				namespaceURI);
+		Node msgId = TestUtil.getNode(header, ns, "wsa:MessageID");
+		assertTrue(msgId.getTextContent().startsWith("BPELUnit-"));
+		Node replyTo = TestUtil.getNode(header, ns, "wsa:ReplyTo/wsa:Address");
 		assertEquals("http://simulated.url", replyTo.getTextContent());
 
-		assertEquals("true", dummyContext.getUserData("WSA-Sent"));
-		assertEquals("WSA-838848474774-3883873747", dummyContext.getUserData("WSA-Sent-ID"));
+		assertEquals("true", activityContext.getUserData("WSA-Sent"));
+		assertTrue(activityContext.getUserData("WSA-Sent-ID").startsWith(
+				"BPELUnit-"));
 
 		// Receive something back (doesn't really matter actually)
 
-		SOAPMessage rcvMessage= factory.createMessage(null, this.getClass().getResourceAsStream(PATH_TO_FILES + "incomingSOAP2.xml"));
-		proc.processReceive(dummyContext, rcvMessage);
-
+		SOAPMessage rcvMessage = factory.createMessage(null, this.getClass()
+				.getResourceAsStream(PATH_TO_FILES + "incomingSOAP2.xml"));
+		wsaProcessor.processReceive(activityContext, rcvMessage);
 	}
 
+	@Test
+	public void testFixedMessageId() throws Exception {
+		String expectedMessageId = "MyNewMessageId";
+		
+		wsaProcessor.setMessageId(expectedMessageId);
+
+		MessageFactory factory = MessageFactory.newInstance();
+		SOAPMessage sendMessage = factory.createMessage(null, this.getClass()
+				.getResourceAsStream(PATH_TO_FILES + "outgoingSOAP.xml"));
+		
+		SendPackage p = new SendPackage("http://target.url", sendMessage);
+		wsaProcessor.processSend(activityContext, p);
+		
+		Element header = p.getSoapMessage().getSOAPHeader();
+		NamespaceContextImpl ns = new NamespaceContextImpl();
+		ns.setNamespace("wsa",
+				NAMESPACE_WSA_2003);
+		Node msgId = TestUtil.getNode(header, ns, "wsa:MessageID");
+		
+		assertEquals(expectedMessageId, msgId.getTextContent());
+	}
 }
