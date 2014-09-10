6d84105fb50e5cdef24d296189a76d54952fe8f4
Daniel Luebke <bpelunit@daniel-luebke.de>
2014-09-10 14:18:29 +0200

    Tidy up imports

--- net.bpelunit.util/src/test/java/net/bpelunit/util/XMLUtilTest.java
+++ net.bpelunit.util/src/test/java/net/bpelunit/util/XMLUtilTest.java
@@ -4,7 +4,6 @@
  */
 package net.bpelunit.util;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
